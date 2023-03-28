802f950b708d3248ff999055b02499ff14278a93
Ahmad Nawaz <106543558+ahmad-an@users.noreply.github.com>
2023-03-28 17:53:31 +0500

    artihmatic operations in TS file

--- ts.ts
+++ ts.ts
@@ -9,3 +9,13 @@ let num2 = 2;
 let result = num1 * num2;
 
 console.log(result);
+
+
+let result2 = num1 + num2;
+console.log(result2);
+
+let result3 = num1 / num2;
+console.log(result3);
+
+let result4 = num1 - num2;
+console.log(result4);
